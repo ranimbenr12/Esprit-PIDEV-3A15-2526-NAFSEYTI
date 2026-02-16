a88c128deab440ce9f5acc7cf9a1ff01f3831020
maram <maram.benmehrez@esprit.tn>
2026-02-16 11:54:16 +0100

    fffff

--- src/main/java/tn/esprit/projet/gui/main.java
+++ src/main/java/tn/esprit/projet/gui/main.java
@@ -14,6 +14,7 @@ public class main extends Application {
         primaryStage.setTitle("NAFSEYTI");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
